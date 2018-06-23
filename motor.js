530c82dfbc0de31ecd91f4b356de06c9e8b388d6
LAPTOP-HGOFS7K3\Sherry Wong <th3bomberry@hotmail.com>
2018-06-23 16:02:53 +0800

    auto

--- motor.js
+++ motor.js
@@ -2,7 +2,7 @@ const Gpio = require('onoff').Gpio;
 
 class Motor { 
     constructor() {
-        this.motor = new Gpio(14, 'out');
+        this.motor = new Gpio(4, 'out');
     }
     startTurn() {
         console.log("on");

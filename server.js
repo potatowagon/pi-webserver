4ad47372ee95bc20175772f618c519eec8724007
LAPTOP-HGOFS7K3\Sherry Wong <th3bomberry@hotmail.com>
2018-08-05 03:41:56 +0800

    auto

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ var temp = 0;
 //settings
 var maxTemp = 38; //initial
 // in ms
-const AUTO_TURN_INTERVAL = 7200000;
+const AUTO_TURN_INTERVAL = 7200000; //2 hours
 const AUTO_TURN_DURATION = 2000;
 const MAX_TEMP_CHECK_INTERVAL = 5000;
 const TEMP_HUMIDITY_POLLING_INTERVAL = 5000;

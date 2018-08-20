e4bf22f0747c3d68f502e8613d0ee7bf6b354ee6
LAPTOP-HGOFS7K3\Sherry Wong <th3bomberry@hotmail.com>
2018-08-21 00:57:06 +0800

    auto

--- js/modal.js
+++ js/modal.js
@@ -2,13 +2,18 @@
 var modal = document.getElementById('myModal');
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('chart');
+var img = [document.getElementById('chart')];
+img.push.apply(img, document.getElementsByClassName('incubator-img')); 
+
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
-img.onclick = function(){
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
+
+for(let i = 0; i < img; i++) {
+    img[i].onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+    }
 }
 
 // Get the <span> element that closes the modal

91bb49f80543098e71e5adaa79f463ffc93bed5e
LAPTOP-HGOFS7K3\Sherry Wong <th3bomberry@hotmail.com>
2018-08-26 13:56:13 +0800

    auto

--- js/info.js
+++ js/info.js
@@ -4,9 +4,8 @@ document.getElementById("day").innerHTML = "Day " + daysSince(new Date(2018,08,1
 //day count 
 function daysSince(startDate) {
     var oneDay = 24*60*60*1000; // hours*minutes*seconds*milliseconds
-    var firstDate = startDate;
-    var secondDate = new Date(); //today
-    return diffDays = Math.round(Math.abs((firstDate.getTime() - secondDate.getTime())/(oneDay)));
+    var today = new Date(); 
+    return diffDays = Math.round(Math.abs((today.getTime() - startDate.getTime())/(oneDay)));
   }
 
   

832923e13590fdcb117adf61f6f5c0f9f38e83b5
LAPTOP-HGOFS7K3\Sherry Wong <th3bomberry@hotmail.com>
2018-08-26 14:05:34 +0800

    auto

--- js/info.js
+++ js/info.js
@@ -1,4 +1,6 @@
-document.getElementById("day").innerHTML = "Day " + daysSince(new Date(2018,07,20)); 
+setInterval(function () {
+    document.getElementById("day").innerHTML = "Day " + daysSince(new Date(2018,07,20));
+}, 21600000);  
 
 
 //day count 
@@ -6,6 +8,6 @@ function daysSince(startDate) {
     var oneDay = 24*60*60*1000; // hours*minutes*seconds*milliseconds
     var today = new Date(); 
     return diffDays = Math.round(Math.abs((today.getTime() - startDate.getTime())/(oneDay)));
-  }
+}
 
   
